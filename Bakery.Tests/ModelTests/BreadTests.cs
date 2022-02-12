beb289ec4c360be1c1d206637ce49b1c30fe0fdf
Amy Young <youngamy1223@gmail.com>
2022-02-11 18:43:42 -0700

    add Bread class & constructor for cost

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfItem_True()
+    {
+      Bread newBread = new Bread("test");
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
+}
---
+++ Bakery/Models/Bread.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+
+namespace Bakery.Models
+{
+  public class Bread
+  {
+    public string BreadCost;
+
+    public Bread(string inputtedBreadCost)
+    {
+      BreadCost = inputtedBreadCost;
+    }
+  }
+}
+
+
