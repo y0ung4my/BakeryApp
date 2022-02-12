0ca540ce023430ec0f9e246c88fa958fad1397eb
Amy Young <youngamy1223@gmail.com>
2022-02-11 23:13:52 -0700

    add buy two get one sale method, update tests

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -8,10 +8,24 @@ namespace Bakery.Tests
   public class BreadTests
   {
     [TestMethod]
-    public void BreadConstructor_CreatesInstanceOfItem_True()
+    public void BreadConstructor_CreatesInstanceOfItem_Int()
     {
-      Bread newBread = new Bread("test");
+      Bread newBread = new Bread(5);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+    [TestMethod]
+    public void BuyTwoGetOneSaleMethod_CalculatesSalePrice_10()
+    {
+      Bread newBread = new Bread(5);
+      int loaves = 3;
+      Assert.AreEqual(10, newBread.BuyTwoGetOneSale(loaves));
+    }
+    [TestMethod]
+    public void BuyTwoGetOneSaleMethod_CalculatesSalePrice_15()
+    {
+      Bread newBread = new Bread(5);
+      int loaves = 4;
+      Assert.AreEqual(15, newBread.BuyTwoGetOneSale(loaves));
     }
   }
 }
--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -6,11 +6,23 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    public string BreadCost;
+    public int BreadCost;
 
-    public Bread(string inputtedBreadCost)
+    public Bread(int inputtedBreadCost)
     {
       BreadCost = inputtedBreadCost;
+    }
+        public int BuyTwoGetOneSale(int numberOfLoaves)
+    {
+      int finalCost = 0;
+      for (int i = 0; i <= numberOfLoaves; i++) 
+      {
+        if ( i % 3 != 0 )
+        {
+          finalCost += BreadCost;
+        }
+      }
+      return finalCost;
     }
   }
 }
